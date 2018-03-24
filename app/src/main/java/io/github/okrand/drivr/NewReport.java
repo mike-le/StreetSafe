57d7c470258bfb5ad42fdb12b058af0b6d66050a
okrand <orkun.krand@gmail.com>
2018-03-24 17:20:16 -0400

    Speech to text Layout

---
+++ app/src/main/java/io/github/okrand/drivr/NewReport.java
@@ -0,0 +1,11 @@
+package io.github.okrand.drivr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Krando67 on 3/24/18.
+ */
+
+public class NewReport extends AppCompatActivity {
+
+}
