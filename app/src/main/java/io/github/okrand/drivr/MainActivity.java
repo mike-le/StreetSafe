7c75a753708936c1ce914761bf0b1775a584b09c
okrand <orkun.krand@gmail.com>
2018-03-24 17:03:01 -0400

    Main Activity in progress

--- app/src/main/java/io/github/okrand/drivr/MainActivity.java
+++ app/src/main/java/io/github/okrand/drivr/MainActivity.java
@@ -2,6 +2,11 @@ package io.github.okrand.drivr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Report> theList = new ArrayList<>();
+
+        ListView myRepsListView = findViewById(R.id.list_my_reports);
+        ArrayAdapter adapter = new ArrayAdapter<Report>(MainActivity.this, R.layout.list_item_record, R.id.list_my_reports, theList){
+
+        };
+        myRepsListView.setAdapter(adapter);
     }
 }
