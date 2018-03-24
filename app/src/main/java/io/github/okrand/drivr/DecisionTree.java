1609b1df246822be9fcfb799d1b97daa8b071a9d
David Promisel <davidp@Davids-MacBook-Pro-4.local>
2018-03-24 16:11:52 -0400

    Decision Tree and Report Class

---
+++ app/src/main/java/io/github/okrand/drivr/DecisionTree.java
@@ -0,0 +1,49 @@
+package io.github.okrand.drivr;
+
+import java.util.List;
+
+public class DecisionTree {
+
+    public String generateReport(List<Integer> input) {
+        int dec1 = input.get(0);
+        int dec2 = input.get(1);
+
+        switch (dec1) {
+            //1 signifies an emergency and is handled in the command manager
+            case 2:
+                return badCar(dec2);
+            case 3:
+                return badDriver(dec2);
+            default:
+                return "";
+        }
+    }
+
+    public String badCar(int dec) {
+        switch (dec) {
+            case 1:
+                return "Inappropriate Speed";
+            case 2:
+                return "Not Paying Attention to Road";
+            case 3:
+                return "Swerving";
+            case 4:
+                return "Tailgating";
+            case 5:
+                return "Did Not Use Turn Signal";
+            default:
+                return "";
+        }
+    }
+
+    public String badDriver(int dec) {
+        switch (dec) {
+            case 1:
+                return "Issue with Lights";
+            case 2:
+                return "Issue with Tire";
+            default:
+                return "";
+        }
+    }
+}
