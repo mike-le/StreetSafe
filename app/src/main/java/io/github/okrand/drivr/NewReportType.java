105714d30cab92aa736e096e4e88e948024d8a19
Stephanie Poinsatte <stephanie.poinsatte@richmond.edu>
2018-03-24 21:43:41 -0400

    NewReportType Screen

--- app/src/main/java/io/github/okrand/drivr/NewReportType.java
+++ app/src/main/java/io/github/okrand/drivr/NewReportType.java
@@ -1,11 +1,11 @@
 package io.github.okrand.drivr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Krando67 on 3/24/18.
- */
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class NewReportType extends AppCompatActivity{
     @Override
@@ -13,6 +13,30 @@ public class NewReportType extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_report_type);
 
+        Button emergency = (Button) findViewById(R.id.emergency);
+        Button driver = (Button) findViewById(R.id.bad_driver);
+        Button car = (Button) findViewById(R.id.bad_car);
+
+        emergency.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewReportType.this, LicensePlate.class);
+            }
+        });
+
+        driver.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewReportType.this, LicensePlate.class);
+            }
+        });
+
+        car.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewReportType.this, LicensePlate.class);
+            }
+        });
     }
 
 }
