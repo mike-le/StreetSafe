5e024053c90315e20964e55bae935adf01b968ff
okrand <orkun.krand@gmail.com>
2018-03-25 10:09:11 -0400

    LAT LON Emergency

--- app/src/main/java/io/github/okrand/drivr/Emergency.java
+++ app/src/main/java/io/github/okrand/drivr/Emergency.java
@@ -1,13 +1,18 @@
 package io.github.okrand.drivr;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class Emergency extends AppCompatActivity{
+public class Emergency extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,21 @@ public class Emergency extends AppCompatActivity{
         Button police = (Button) findViewById(R.id.police);
         Button fire = (Button) findViewById(R.id.fire);
         Button ems = (Button) findViewById(R.id.ems);
+
+        LocationManager lm = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
 
         police.setOnClickListener(new OnClickListener() {
             @Override
